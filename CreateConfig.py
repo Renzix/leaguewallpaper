8a67d8022b21dc1813f3a068dcb714a672b9bc96
renzix <eragon9981@gmail.com>
2017-09-06 11:36:25 -0400

    On branch master  Changes to be committed: 	modified:   CreateConfig.py 	modified:   config.txt

--- CreateConfig.py
+++ CreateConfig.py
@@ -4,30 +4,30 @@ f.write("LeagueName:"+input("Enter your Username: ")+"\n");
 f.write("CurrentChamp:Veigar\n");
 f.write("Region:");
 Region=input("Enter Region:");
-if(Region=="North America"||Region=="NA"):
+if(Region=="North America" or Region=="NA"):
     f.write("NA1");
-elif(Region=="Europe West"||Region=="EUW"):
+elif(Region=="Europe West" or Region=="EUW"):
     f.write("EUW1");
-elif(Region=="Europe North East"||Region=="EUNE"):
+elif(Region=="Europe North East" or Region=="EUNE"):
     f.write("EUN1");
-elif(Region=="Brazil"||Region=="BR"):
+elif(Region=="Brazil" or Region=="BR"):
     f.write("BR1");
-elif(Region=="Japan"||Region=="JP"):
+elif(Region=="Japan" or Region=="JP"):
     f.write("JP1");
-elif(Region=="Korea"||Region=="KR"):
+elif(Region=="Korea" or Region=="KR"):
     f.write("KR");
-elif(Region=="Latin America North"||Region=="LAN"):
+elif(Region=="Latin America North" or Region=="LAN"):
     f.write("LA1");
-elif(Region=="Latin America South"||Region=="LAS"):
+elif(Region=="Latin America South" or Region=="LAS"):
     f.write("LA2");
-elif(Region=="Oceania"||Region=="OCE"):
+elif(Region=="Oceania" or Region=="OCE"):
     f.write("OC1");
-elif(Region=="Turkey"||Region=="TR"):
+elif(Region=="Turkey" or Region=="TR"):
     f.write("TR1");
-elif(Region=="Russia"||Region=="RU"):
+elif(Region=="Russia" or Region=="RU"):
     f.write("RU");
-elif(Region=="Public Beta Environment"||Region=="PBE"):
+elif(Region=="Public Beta Environment" or Region=="PBE"):
     f.write("PBE1");
-else
+else:
     print("Error finding region please Enter the initals of your region in capital letters ie NA or EUW");
 f.close();
